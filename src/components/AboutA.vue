fe9323dd30b13029bfffc55bdd2035e347a8de16
Floor Schouten <corporatecontent@outlook.com>
2021-03-23 13:25:02 +0100

    Added articles data and used V-for to display About

--- src/components/AboutA.vue
+++ src/components/AboutA.vue
@@ -1,17 +1,49 @@
 <template>
-    <div>
-        <div> 
-            <h1>{{title}}</h1>
+    <div class="contentbox1">
+
+        <div v-for="article in articlesList" v-bind:key="article.id">
+            <div class="headerbox">  
+                <div class="iconAbout">
+                    <i class="fas fa-hat-cowboy-side"></i>
+                </div>
+                <div>
+                    <ChildComponent v-model="title" />
+                    <h1>{{ article.title }}</h1>
+                    <p>{{ article.body }}</p>
+                </div>
             </div>
+        </div>
     </div>
 </template>
 
 <script>
-export default {
-    
+import articles from '../assets/articles.json';
+
+export default{
+    data() {
+        return {
+            articlesList: articles
+        }
+    }
 }
 </script>
 
 <style scoped>
+.contentbox1 {
+    display: flex;
+    flex-direction: column;
+    width: 800px;
+    height: auto;
+    background-color: #eeebdd;
+    margin: 10px 100px;
+    padding-left: 50px;
+    padding-left: 50px;
+    box-shadow: 2px 3px 3px 3px red;
+    border-radius: 25px; 
+}
+.iconAbout{
+    display: flex;
+    font-size: xx-large;
 
+}
 </style>
---
+++ src/components/Contact.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <div class="Contentbox">
+        <input v-model="message" placeholder="edit me">
+        <p>Message is: {{ message }}</p>
+            
+        </div>
+        </div>
+</template>
+
+<script>
+
+
+export default {
+    name: 'Contact'
+}
+</script>
+
+<style scoped>
+
+</style>
